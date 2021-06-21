649110bcdbc00882cf97414b005486e2827cfc0b
rahulragava <rahulragava3@gmail.com>
2021-06-21 07:18:37 -0700

    changed px to percentage

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 
 const App = () =>
 	<div className="App">
-		<div className="circle logo">
-			<div className="box b1"/>
-			<div className="box b2"/>
-			<div className="box b3"/>
-			<div className="box b4"/>
+		<div className="container">
+			<div className="circle">
+				<div className="logo">
+					<div className="box b1"/>
+					<div className="box b2"/>
+					<div className="box b3"/>
+					<div className="box b4"/>
+				</div>
+			</div>
 		</div>
+
 	</div>
 ;
 
